59d03a2909e07ad6084e425122d632b9934281b1
Rafael Gutiérrez <rafagutibarrio@gmail.com>
2026-05-25 13:01:57 -0600

    Proyecto Base con dependencias y conexión a BD

---
+++ Parcial2_PNC/src/main/java/uca/ncapas/parcial2_pnc/Parcial2PncApplication.java
@@ -0,0 +1,13 @@
+package uca.ncapas.parcial2_pnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2PncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcial2PncApplication.class, args);
+    }
+
+}
---
+++ Parcial2_PNC/src/test/java/uca/ncapas/parcial2_pnc/Parcial2PncApplicationTests.java
@@ -0,0 +1,13 @@
+package uca.ncapas.parcial2_pnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2PncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
